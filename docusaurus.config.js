acf68f8922208baa8dc073d45d03c6610639d4ff
tcnam <tcnam2000@gmail.com>
2025-11-30 07:49:40 +0700

    update for test

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,15 +20,15 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://tcnam.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/book_reviews/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'tcnam', // Usually your GitHub org/user name.
+  projectName: 'book_reviews', // Usually your repo name.
   deploymentBranch: 'master',
 
   onBrokenLinks: 'throw',
