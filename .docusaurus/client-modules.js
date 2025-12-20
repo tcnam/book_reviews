1326b1094b69030b8eac76396d4d3eade77334f5
tcnam <tcnam2000@gmail.com>
2025-12-20 18:24:02 +0700

    update

--- .docusaurus/client-modules.js
+++
@@ -1,7 +0,0 @@
-export default [
-  require("/Users/namtran/repos/book_reviews/.docusaurus/docusaurus-plugin-css-cascade-layers/default/layers.css"),
-  require("/Users/namtran/repos/book_reviews/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/namtran/repos/book_reviews/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/Users/namtran/repos/book_reviews/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/Users/namtran/repos/book_reviews/src/css/custom.css"),
-];
